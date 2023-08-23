f05f3da5ebeb38abd0bd62fb93a42fd2e1855771
stautisabela <stautisabela@gmail.com>
2023-08-23 10:04:35 +0200

    Created main blueprint and rendered template

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,12 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    # Flask extensions
+
     # Blueprints
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
     @app.route('/test/')
     def test_page():
         return '<h1>Testing Flask application</h1>'
