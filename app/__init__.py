0b9ce9b47aa2e81230b52e687490cf99ae55573b
stautisabela <stautisabela@gmail.com>
2023-08-23 10:39:35 +0200

    added Posts and Categories to the application

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,9 @@ def create_app(config_class=Config):
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
+    from app.posts import bp as posts_bp
+    app.register_blueprint(posts_bp, url_prefix='/posts')
+
     @app.route('/test/')
     def test_page():
         return '<h1>Testing Flask application</h1>'
